1d5c357b126fbbb7a04bee4ca3918f2f1d37a468
aman.talwar <aman.talwar@samsung.com>
2024-10-08 15:04:38 +0530

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,14 +35,16 @@ android {
 
 dependencies {
 
+	implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.20")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 	implementation("androidx.core:core-ktx:1.13.1")
 	implementation("androidx.appcompat:appcompat:1.6.1")
 	implementation("com.google.android.material:material:1.12.0")
 	implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
 	// ONNX
-	implementation("com.microsoft.onnxruntime:onnxruntime-android:1.18.0")
-	implementation("com.microsoft.onnxruntime:onnxruntime-training-android:1.18.0")
-	implementation("com.microsoft.onnxruntime:onnxruntime-extensions-android:0.9.0")
+	implementation("com.microsoft.onnxruntime:onnxruntime-android:1.19.2")
+	implementation("com.microsoft.onnxruntime:onnxruntime-training-android:1.19.2")
+	implementation("com.microsoft.onnxruntime:onnxruntime-extensions-android:0.12.4")
 
 }
