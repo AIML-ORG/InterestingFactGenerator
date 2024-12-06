0573b730da213389d7453f8dafa222e60091e157
Aman Talwar <Aman.Talwar@Samsung.com>
2024-12-06 16:03:00 +0530

    gradle update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
 	namespace = "com.example.myapplication"
-	compileSdk = 34
+	compileSdk = 35
 
 	defaultConfig {
 		applicationId = "com.example.myapplication"
-		minSdk = 32
-		targetSdk = 34
+		minSdk = 33
+		targetSdk = 35
 		versionCode = 1
 		versionName = "1.0"
 	}
@@ -18,7 +18,10 @@ android {
 	buildTypes {
 		release {
 			isMinifyEnabled = false
-			proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+			proguardFiles(
+				getDefaultProguardFile("proguard-android-optimize.txt"),
+				"proguard-rules.pro"
+			)
 		}
 	}
 	compileOptions {
@@ -35,16 +38,16 @@ android {
 
 dependencies {
 
-	implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.20")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib:2.1.0")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
-	implementation("androidx.core:core-ktx:1.13.1")
-	implementation("androidx.appcompat:appcompat:1.6.1")
+	implementation("androidx.core:core-ktx:1.15.0")
+	implementation("androidx.appcompat:appcompat:1.7.0")
 	implementation("com.google.android.material:material:1.12.0")
-	implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+	implementation("androidx.constraintlayout:constraintlayout:2.2.0")
 
 	// ONNX
-	implementation("com.microsoft.onnxruntime:onnxruntime-android:1.19.2")
+	implementation("com.microsoft.onnxruntime:onnxruntime-android:1.20.0")
 	implementation("com.microsoft.onnxruntime:onnxruntime-training-android:1.19.2")
-	implementation("com.microsoft.onnxruntime:onnxruntime-extensions-android:0.12.4")
+	implementation("com.microsoft.onnxruntime:onnxruntime-extensions-android:0.13.0")
 
 }
